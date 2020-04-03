aa9b7d1395459d3bb5388466c8b9b081034778e0
Josh Taylor <josh@magmo.co.uk>
2020-04-03 17:18:58 +0100

    Add buttons

--- components/index/Intro.tsx
+++ components/index/Intro.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { ChildFlexSection } from '../global/Containers';
 import { H1 } from '../global/Typography';
@@ -6,6 +7,9 @@ import { black } from '../utilities/Colors';
 const Intro = () => (
   <ChildFlexSection index={1} backing={black} padding="4rem">
     <H1>IN PROGRESS...</H1>
+    <Link href="/example">
+      <a>example</a>
+    </Link>
   </ChildFlexSection>
 );
 
---
+++ components/inputs/buttons.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { smoke } from '../utilities/Colors';
+
+type ButtonProps = {
+  stretch: boolean;
+};
+
+export const Button = styled.button<ButtonProps>`
+  width: ${(props) => (props.stretch ? '100%' : 'auto')};
+  padding: 1rem;
+  font-size: 12px;
+  font-weight: bold;
+  color: #31302b;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  cursor: pointer;
+  background-color: ${smoke};
+  border-radius: 0.2rem;
+  box-shadow: inset 0 0 0 0 #31302b;
+  transition: all ease 0.8s;
+
+  &:active {
+    transform: scale(0.95);
+  }
+
+  &:hover {
+    color: #fff;
+    box-shadow: inset 300px 0 0 0 #424242;
+  }
+`;
