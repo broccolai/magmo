98e70774e6efc6991ed57391d0003db7fcb1da98
broccolai <me@broccol.ai>
2021-04-29 22:11:16 +0100

    Preconnect to tickets subdomain

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import Layout from '../components/hocs/Layout';
 import Intro from '../components/index/Intro';
@@ -5,10 +6,15 @@ import Splash from '../components/index/Splash';
 import { black } from '../components/utilities/Colors';
 
 const Index = () => (
-  <Layout title="Home" backing={black}>
-    <Splash />
-    <Intro />
-  </Layout>
+  <>
+    <Head>
+      <link rel="preconnect" href="https://tickets.broccol.ai" />
+    </Head>
+    <Layout title="Home" backing={black}>
+      <Splash />
+      <Intro />
+    </Layout>
+  </>
 );
 
 export default Index;
