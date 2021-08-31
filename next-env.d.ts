2abd67fb36ccc7872d650bcbbd071470ac4222ea
broccolai <me@broccol.ai>
2021-08-31 01:45:02 +0100

    chore: version bumps

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,40 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-interface PropertiesItemType {
-  startValue: string | number;
-  endValue: string | number;
-  property: string;
-  unit?: string;
-}
-
-interface ParallaxDataType {
-  start: string | number | SafeHTMLElement;
-  startOffset?: string | number;
-  duration?: string | number | SafeHTMLElement;
-  end?: string | number | SafeHTMLElement;
-  endOffset?: string | number;
-  properties: PropertiesItemType[];
-  easing?: string | array | func;
-  name?: string;
-}
-
-interface IPlxPros {
-  animateWhenNotInViewport?: boolean;
-  children?: any;
-  className?: string;
-  disabled?: boolean;
-  freeze?: boolean;
-  parallaxData: ParallaxDataType[];
-  style?: string | number | object;
-  tagName?: string;
-  onPlxStart?: PropTypes.func;
-  onPlxEnd?: PropTypes.func;
-}
-
-declare module 'react-plx' {
-  declare class Plx extends React.Component<IPlxPros> {
-    constructor(props: IPlxPros);
-  }
-  export = Plx;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
